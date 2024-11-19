2be4f2e64398854925dc4184e96503d12f6b7b40
Amatrasu66 <135813973+Amatrasu66@users.noreply.github.com>
2024-11-19 20:42:11 +0530

    Create print hello world nth number of timers.c
    a c program code that prints the word hello word nth number of times

---
+++ print hello world nth number of timers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("enter the number : ");
+    scanf("%d",&num);
+    
+    for(int i = 0; i <= num; i++)
+    {
+        printf("Hello world\n");
+    }
+    return 0;
+}
